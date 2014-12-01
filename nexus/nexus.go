6b80363ba6dbbb279ae2dc2dcc8552f93a5cd222
Mark Petrovic <mark.petrovic@xoom.com>
2014-12-01 14:22:17 -0800

    code and test repo-exists

--- nexus/nexus.go
+++ nexus/nexus.go
@@ -1,4 +1,47 @@
 package nexus
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Client struct {
+	baseURL    string // http://localhost:8081/nexus
+	username   string
+	password   string
+	httpClient *http.Client
+}
+
 func init() {
+}
+
+func NewClient(baseURL, username, password string) *Client {
+	client := &http.Client{}
+	return &Client{baseURL, username, password, client}
+}
+
+func (client *Client) RepositoryExists(repositoryID string) (bool, error) {
+	req, err := http.NewRequest("GET", client.baseURL+"/service/local/repositories/"+repositoryID, nil)
+	if err != nil {
+		return false, err
+	}
+	req.SetBasicAuth(client.username, client.password)
+	req.Header.Add("Accept", "application/json")
+
+	resp, err := client.httpClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	if _, err := ioutil.ReadAll(resp.Body); err != nil {
+		return false, err
+	}
+
+	if resp.StatusCode != 200 && resp.StatusCode != 404 {
+		return false, fmt.Errorf("Client.RepositoryExists(): unexpected response status: %d\n", resp.StatusCode)
+	}
+
+	return resp.StatusCode == 200, nil
 }
