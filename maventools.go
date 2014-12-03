1a0b9ab9ee8438ba3796491412cd2c89ff5bcd47
Mark Petrovic <mark.petrovic@xoom.com>
2014-12-03 05:52:16 -0800

    rebase/merge with develop

---
+++ maventools.go
@@ -0,0 +1,42 @@
+package maventools
+
+import "net/http"
+
+type RepositoryID string
+
+type GroupID string
+
+type Repository struct {
+	ID   RepositoryID
+	Name string
+}
+
+// Group models a repository group.
+type Group struct {
+	ID           GroupID
+	Name         string
+	Repositories []Repository
+}
+
+type Client struct {
+	ClientOps
+	baseURL    string
+	username   string
+	password   string
+	httpClient *http.Client
+}
+
+// ClientOps defines the service methods on a Client. Integer return values are the underlying HTTP response codes.
+type ClientOps interface {
+	RepositoryExists(RepositoryID) (bool, error)
+	CreateSnapshotRepository(RepositoryID) (int, error)
+	DeleteRepository(RepositoryID) (int, error)
+	AddRepositoryToGroup(RepositoryID, GroupID) (int, error)
+	RemoveRepositoryFromGroup(RepositoryID, GroupID) (int, error)
+}
+
+/*
+func NewNexusClient(baseURL, username, password string) nexus.Client {
+	return nexus.Client{}
+}
+*/
